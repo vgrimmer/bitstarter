632064b3ceb3209f25ace1f337f5e884cc0c8e48
Kuangye Guo <kuangye.guo@gmail.com>
2013-07-06 22:14:46 +0000

    Adding index.html and read input from there.

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var display_str = fs.readFileSync("index.html");
+
 app.get('/', function(request, response) {
-  response.send('Hey there!');
+    response.send(display_str.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
